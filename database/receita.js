c3378cad8b03ff871a8354bf1f88d6f143b4bae2
Reinaldo Themistocles <rdthemis@gmail.com>
2023-06-07 10:09:41 -0300

    criado rota de get e put receita

--- database/receita.js
+++ database/receita.js
@@ -1,11 +1,41 @@
 const prisma = require("./prisma");
 
-const saveReceita = (receita) => {
+const saveReceita = (receita, userId) => {
     return prisma.receita.create({
-        data: receita,
-    })
+        data: {
+            descricao: receita.descricao,
+            nome: receita.nome,
+            tempoPreparo: receita.tempoPreparo,
+            user: {
+                connect: {
+                    id: userId,
+                },
+            },
+        },
+    });
 }
+
+const buscaReceitas = (userId) => {
+    return prisma.receita.findMany({
+        where: {
+            userId: userId,
+        },
+    });
+}
+
+const atualizarReceita = (receita, id, userId) => {
+    return prisma.receita.updateMany({
+        data: receita,
+        where: {
+            id: id,
+            userId: userId
+        },
+    });
+}
+
 
 module.exports = {
     saveReceita,
+    buscaReceitas,
+    atualizarReceita
 }
